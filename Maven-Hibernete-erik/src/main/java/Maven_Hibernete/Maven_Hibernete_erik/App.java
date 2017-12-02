42ae043f3744226b497a9eaaade00ef619d875c6
Erik Markovič <erik.markovic@seznam.cz>
2017-12-02 16:34:29 +0100

    Commit - one

---
+++ Maven-Hibernete-erik/src/main/java/Maven_Hibernete/Maven_Hibernete_erik/App.java
@@ -0,0 +1,13 @@
+package Maven_Hibernete.Maven_Hibernete_erik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

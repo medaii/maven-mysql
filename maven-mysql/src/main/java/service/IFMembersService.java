15415439b77af4a6e0c1426ed2c794bb6114f2e4
Erik Markovič <erik.markovic@seznam.cz>
2018-02-14 11:47:43 +0100

    Commit - edit JavaDoc MembersService

--- maven-mysql/src/main/java/service/IFMembersService.java
+++ maven-mysql/src/main/java/service/IFMembersService.java
@@ -12,87 +12,241 @@ import loko.value.MemberFull;
 import loko.value.MemberList;
 import loko.value.PhonesMeber;
 
+/**
+*
+* Servisní tøída pro DAO (obstaravající skupinu seznam èlenù)
+* 
+* @author Erik Markovic
+*
+*
+*/
+
 public interface IFMembersService {
 
 	/**
-	 * servis MembersDAO metod
-	 */
-
-	void deleteMember(int id);
-
-	int addMemberFull(MemberFull member);
-
-	void updateMember(Member member, int id);
-
-	void updateMember(MemberFull member, int id);
-
-	List<Member> getAllMember();
-
-	List<MemberList> getAllMemberList(boolean active, int kategorie);
-
-	List<MemberList> searchAllMembers(String name, boolean active, int kategorie);
-
-	Member getMember(int id);
-
-	MemberFull getMemberFull(int id);
-
-	/**
-	 * servis PhoneDAO
-	 */
-	void deletePhone(int id);
-
-	int addPhone(Phone phone);
-
-	void updatePhone(Phone phone, int id);
-
-	Map<Integer, PhonesMeber> getAllPhonesMembers();
-
-	PhonesMeber getPhonesMember(int id_member);
-
-	Phone getPhone(int id);
-
-	/**
-	 * servis MailDAO
-	 */
-	void deleteMail(int id);
-
-	int addMail(Mail mail);
-
-	void updateMail(Mail mail, int id);
-
-	Map<Integer, MailsMember> getAllMailMembers();
-
-	MailsMember getMailsMember(int id_member);
-
-	Mail getMail(int id);
-
-	/**
-	 * metody UserDAO
+	 * metody poskytnute MembersDAO
 	 */
 	
 	/**
+	 * Vymazání entity Member
 	 * 
-	 * @param theUser - Entita u které byli provedené zmìny
+	 * @param id -  id entity pro smazáni z DB
+	 * 
 	 */
-	void updateUser(User theUser);
+	public void deleteMember(int id);
 
-	void changePassword(User theUser, String newPassword);
-
-	List<User> getUsers(boolean admin, int userId);
-
-	/** 
-	 * @return kontrola shody zadaného hesla a hesla v DB
+	/**
+	 * Pøijmutí pøepravky MemberFull, kde jsou hodnoty pro tvorbu nových entit Member, CSHRegNumber, RodneCislo,
+	 * 					 TrvaleBydliste.
+	 * 
+	 * @param - MemberFull je pøepravka hodnot pro entity Member, CSHRegNumber, RodneCislo,
+	 * 					 TrvaleBydliste.
 	 */
-	boolean authenticate(byte[] password, int id); 
+	public int addMemberFull(MemberFull member);
 	
 	/**
-	 * @password -  heslo nezakodovane v poli byte
-	 * return zakodované heslo
+	 * Aktualizace udaju v entite Member v DB
 	 */
-	public String encryptPassword(byte[] password);
+	public void updateMember(Member member, int id);
 	
-	int addUser(User theUser);
+	/**
+	 * Aktualizace údajù pomoci pøepravky v entitách Member, CSHRegNumber, RodneCislo,
+	 * 					 TrvaleBydliste.
+	 * @param - MemberFull je pøepravka hodnot pro entity Member, CSHRegNumber, RodneCislo,
+	 * 					 TrvaleBydliste.
+	 *  
+	 */
+	public void updateMember(MemberFull member, int id);
 
+	/**
+	 * Pro nactení všech èlenù v seznamu èlenù v DB
+	 * 
+	 * @return - vrací list naplnìný všemi entitami Member z DB
+	 */
+	public List<Member> getAllMember();
 	
+	/**
+	 * Pro naètení entit Member, Mail, Phone dle parametru active z DB
+	 * 
+	 * @param active - pri true vypíše jen aktivní èleny
+	 * 
+	 * @param kategorie - možnost filtru dle vìkové kategorie
+	 * 
+	 *@return - vraci pøepravku MemberList, který obsahuje Member, Mail a Phone
+	 *
+	 */
+	public List<MemberList> getAllMemberList(boolean active, int kategorie);
+	
+	/**
+	 * Funkce vraci seznam èlenu dle hledaných znaku v køesním jménì nebo pøíjmení
+	 * 
+	 * @param name - hledané znaky
+	 * 
+	 * @param active - možnost filtru dle vìkové kategorie
+	 * 
+	 * @param kategorie -  možnost filtru dle vìkové kategorie
+	 */
+	public List<MemberList> searchAllMembers(String name, boolean active, int kategorie);
+	
+	/**
+	 * Vrací entitu Member z DB
+	 * 
+	 * @param id - id požadované entity z DB
+	 * 
+	 * @return Member - vrací požadovanou entitu z DB
+	 */
+	public Member getMember(int id);
+	
+	/**
+	 * Vraci požadovanou pøepravku s hodnotami z entit Member,, CSHRegNumber, RodneCislo,
+	 * 					 TrvaleBydliste.
+	 * 
+	 * @param id - id Member pro kterého má být pøipravena pøepravka 
+	 * 
+	 * @return MemberFull - je pøepravka hodnot pro entity Member, CSHRegNumber, RodneCislo,
+	 * 					 TrvaleBydliste.
+	 */
+	public MemberFull getMemberFull(int id);
+
+	/*
+	 * Metody poskytující  PhoneDAO
+	 */
+	
+	/**
+	 * Smazání entity Phone v DB
+	 * 
+	 * @param id - id entity Phone 
+	 * 
+	 */
+	public void deletePhone(int id);
+	
+	/**
+	 * Pøidaní entity Phone do DB
+	 * 
+	 * @param Phone -  entita Phone, která má být pøidaná do DB
+	 */
+	public int addPhone(Phone phone);
+	
+	/**
+	 * Aktualizace udajù entity Phone v DB
+	 * 
+	 * @param phone - entita,kterou má být nahrazen záznam v DB
+	 * 
+	 */
+	public void updatePhone(Phone phone, int id);
+	
+	/**
+	 * Vraceni seznamu telefoních èísel v mapì podle klíèe id Member
+	 * 
+	 * @return Map<Integer, PhonesMeber> - vrací mapu s klíèem id Member a hodnotou seznam telefoních èísel
+	 */
+	public Map<Integer, PhonesMeber> getAllPhonesMembers();
+	
+	/**
+	 * Vrací pøepravku naplnìnou entitou Member listem telefoních èísel, který jsou s ním ve vztahu
+	 * 
+	 * @param id Memeber - id member který požadujeme
+	 * 
+	 * @return PhonesMeber - vrací pøepravku naplnìnou entitou Member listem telefoních èísel, který jsou s ním ve vztahu
+	 */
+	public PhonesMeber getPhonesMember(int id_member);
+	
+	/**
+	 * Vrací požadovanou entitu Phone z DB dle id Phone
+	 * 
+	 * @param id - Id Phone
+	 * 
+	 */
+	public Phone getPhone(int id);
+	
+	//Metody poskytující  MailDAO	
+	/**
+	 * Vymaže požadovanou entitu z DB dle id Mail
+	 * 
+	 * @param id -  id Mail
+	 */
+	public void deleteMail(int id);
+	
+	/**
+	 * Pøidá novou entitu Mail do DB
+	 * 
+	 * @param mail - Entita, která má být pøidaná do DB
+	 */
+	public int addMail(Mail mail);
+
+	/**
+	 * Aktualizace udajù entity Mail v DB
+	 * 
+	 * @param mail - Entita Mail, kterou má být nahrazen záznam v DB
+	 * 
+	 * @param id - id Mail
+	 */
+	public void updateMail(Mail mail, int id);
+
+	/**
+	 * Vraceni seznamu mailù v mapì podle klíèe id Member
+	 * 
+	 * @return Map<Integer, MailsMeber> - vrací mapu s klíèem id Member a hodnotou seznam mailù
+	 */
+	public Map<Integer, MailsMember> getAllMailMembers();
+
+	/**
+	 * Vrací pøepravku naplnìnou entitou Member listem mailù, který jsou s ním ve vztahu
+	 * 
+	 * @param id Memeber - id member který požadujeme
+	 * 
+	 * @return MailsMeber - vrací pøepravku naplnìnou entitou Member listem mailù, který jsou s ním ve vztahu
+	 */
+	public MailsMember getMailsMember(int id_member);
+	
+
+	/**
+	 * Vrací požadovaou entitu Mail z DB
+	 * 
+	 * @param id - id Mail poždované entity
+	 * 
+	 * @return Mail - požadovaná entita
+	 */
+	public Mail getMail(int id);
+	
+	//user service
+	//TODO pøesunout do samostatného servisu
+
+	/**
+	 * Aktualizace udajù entity User v DB
+	 * 
+	 * @param theUser - Entita User, kterou má být nahrazen záznam v DB
+	 */
+	public void updateUser(User theUser);
+
+	/**
+	 * Zmìna stavajícího hesla za nové
+	 * 
+	 * @param theUser -  Entita, kde má být provedena zmìna
+	 */
+	public void changePassword(User theUser, String newPassword);
+	
+	/**
+	 * Vraci seznam všech entit User v DB
+	 * 
+	 * @param userId - Dle pøistupových práv se filtruje seznam pøistupných entit z tabulky users
+	 * 
+	 * @return List<User> - Seznam entit User
+	 * 
+	 */
+	public List<User> getUsers(boolean admin, int userId);
+	/** 
+	 * Kontrola shody zadaného hesla a hesla v DB
+	 * 
+	 * @return boolean - true znamena shodu
+	 */
+	public boolean authenticate(byte[] password,int id);
+	
+	/**
+	 * Pøidaní nové entity User do DB
+	 * 
+	 * @param theUser - Entita User, která má být pøidaná do DB
+	 */
+	public int addUser(User theUser) ;
 
 }
--- maven-mysql/src/main/java/service/MembersServiceImpl.java
+++ maven-mysql/src/main/java/service/MembersServiceImpl.java
@@ -17,20 +17,22 @@ import loko.value.MemberFull;
 import loko.value.MemberList;
 import loko.value.PhonesMeber;
 /**
+ *
+ * Servisní tøída pro DAO (obstaravající skupinu seznam èlenù)
  * 
  * @author Erik Markovic
  *
- *Servisní tøída pro DAO
+ *
  */
 public class MembersServiceImpl implements IFMembersService {
-
+	
+	// instance na DAO dle entit
 	private MembersDAO membersDAO;
 	private MailsDAO mailsDAO;
 	private PhoneDAO phoneDAO;
 	private UserDAO userDAO;
 
-	//membersDAO = DAOFactory.createDAO(IFMembersDAO.class);
-	
+	// instance se žádají od factory, která posle instanci na DAO pomoci JDBC nebo Hibernate
 	public MembersServiceImpl() {
 		membersDAO = (MembersDAO)DAOFactory.createDAO(MembersDAO.class);
 		this.mailsDAO = (MailsDAO)DAOFactory.createDAO(MailsDAO.class);
@@ -38,218 +40,149 @@ public class MembersServiceImpl implements IFMembersService {
 		this.userDAO = (UserDAO)DAOFactory.createDAO(UserDAO.class); 
 		
 	}
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#deleteMember(int)
+	/*
+	 * metody poskytnute MembersDAO
 	 */
 	
-	// mmembers service
 	@Override
 	public void deleteMember(int id) {
 		membersDAO.deleteMember(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#addMemberFull(loko.core.MemberFull)
-	 */
 	@Override
 	public int addMemberFull(MemberFull member) {
 		return membersDAO.addMemberFull(member);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#updateMember(loko.core.Member, int)
-	 */
 	@Override
 	public void updateMember(Member member, int id) {
 		membersDAO.updateMember(member, id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#updateMember(loko.core.MemberFull, int)
-	 */
 	@Override
 	public void updateMember(MemberFull member, int id) {
 		membersDAO.updateMemberFull(member, id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getAllMember()
-	 */
 	@Override
 	public List<Member> getAllMember() {
 		return membersDAO.getAllMember();
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getAllMemberList(boolean, int)
-	 */
 	@Override
 	public List<MemberList> getAllMemberList(boolean active, int kategorie) {
 		return membersDAO.getAllMemberList(active, kategorie);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#searchAllMembers(java.lang.String, boolean, int)
-	 */
 	@Override
 	public List<MemberList> searchAllMembers(String name, boolean active, int kategorie) {
+		//TODO dodìlat v implementaci filtrace dle parametru active a kategorie
 		return membersDAO.searchAllMembers(name, active, kategorie);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getMember(int)
-	 */
 	@Override
 	public Member getMember(int id) {
 		return membersDAO.getMember(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getMemberFull(int)
-	 */
 	@Override
 	public MemberFull getMemberFull(int id) {
 		return membersDAO.getMemberFull(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#deletePhone(int)
+	/*
+	 * Metody poskytující  PhoneDAO
 	 */
 	
-	// phone service
 	@Override
 	public void deletePhone(int id) {
 		phoneDAO.deletePhone(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#addPhone(loko.core.Phone)
-	 */
 	@Override
 	public int addPhone(Phone phone) {
 		return phoneDAO.addPhone(phone);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#updatePhone(loko.core.Phone, int)
-	 */
 	@Override
 	public void updatePhone(Phone phone, int id) {
+		//TODO vyøešit nadbiteènost parametru id
 		 phoneDAO.updatePhone(phone, id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getAllPhonesMembers()
-	 */
 	@Override
 	public Map<Integer, PhonesMeber> getAllPhonesMembers() {
 		return phoneDAO.getAllPhonesMembers();
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getPhonesMember(int)
-	 */
 	@Override
 	public PhonesMeber getPhonesMember(int id_member) {
 		return phoneDAO.getPhonesMember(id_member);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getPhone(int)
-	 */
 	@Override
 	public Phone getPhone(int id) {
 		return phoneDAO.getPhone(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#deleteMail(int)
-	 */
+	//Metody poskytující  MailDAO	
+	
 	@Override
 	public void deleteMail(int id) {
 		mailsDAO.deleteMail(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#addMail(loko.core.Mail)
-	 */
-	
-	//mail sevice
 	@Override
 	public int addMail(Mail mail) {
 		return mailsDAO.addMail(mail);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#updateMail(loko.core.Mail, int)
-	 */
+
 	@Override
 	public void updateMail(Mail mail, int id) {
 		mailsDAO.updateMail(mail, id);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getAllMailMembers()
-	 */
 	@Override
 	public Map<Integer, MailsMember> getAllMailMembers() {
 		return mailsDAO.getAllMailMembers();
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getMailsMember(int)
-	 */
 	@Override
 	public MailsMember getMailsMember(int id_member) {
 		return mailsDAO.getMailsMember(id_member);
 	}
 
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getMail(int)
-	 */
+
 	@Override
 	public Mail getMail(int id) {
 		return mailsDAO.getMail(id);
 	}
 	
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#updateUser(loko.core.User)
-	 */
-	
 	//user service
+	//TODO pøesunout do samostatného servisu
+
 	@Override
 	public void updateUser(User theUser) {
 		userDAO.updateUser(theUser);
 	}
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#changePassword(loko.core.User, java.lang.String)
-	 */
+
 	@Override
 	public void changePassword(User theUser, String newPassword) {
 		userDAO.changePassword(theUser, newPassword);
 	}
-	/* (non-Javadoc)
-	 * @see service.IFMembersService#getUsers(boolean, int)
-	 */
+	
+
 	@Override
 	public List<User> getUsers(boolean admin, int userId) {
 		return userDAO.getUsers(admin, userId);
 	}
-	/** 
-	 * @return kontrola shody zadaného hesla a hesla v DB
-	 */
+
 	@Override
 	public boolean authenticate(byte[] password,int id) {
 		return userDAO.authenticate(password, id);
 	}
-	/**
-	 * @password -  heslo nezakodovane v poli byte
-	 * return zakodované heslo
-	 */
-	public String encryptPassword(byte[] password) {
-		return PasswordUtils.encryptPassword(password);
-	}
-	
+
 	@Override
 	public int addUser(User theUser) {		
 		return userDAO.addUser(theUser);
